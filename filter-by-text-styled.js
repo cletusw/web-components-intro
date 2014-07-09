0ef34cefe72b88e764405c93c817bb917a55028c
Clayton Watts <cletusw@gmail.com>
2014-07-09 13:04:59 -0600

    Introduce Shadow DOM

--- filter-by-text-styled.js
+++ filter-by-text-styled.js
@@ -3,13 +3,13 @@ var FilterByTextStyledElement = (function() {
   var template = document.currentScript.previousElementSibling;
   var FilterByTextStyledProto = Object.create(FilterByTextElement.prototype);
 
-  FilterByTextStyledProto.setupDom = function() {
-    FilterByTextElement.prototype.setupDom.call(this);
+  FilterByTextStyledProto.createdCallback = function() {
+    FilterByTextElement.prototype.createdCallback.call(this);
 
     var clone = document.importNode(template.content, true);
     this.filterTextElement = clone.querySelector('.filterText');
 
-    this.insertBefore(clone, this.firstChild);
+    this.createShadowRoot().appendChild(clone);
   };
 
   FilterByTextStyledProto.filter = function(filterText) {
