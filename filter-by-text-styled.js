12139c5afb44486f282e1b76c497f4fb09c10d1c
Clayton Watts <cletusw@gmail.com>
2014-07-10 13:43:42 -0600

    Introduce automatic node finding

--- filter-by-text-styled.js
+++ filter-by-text-styled.js
@@ -1,15 +1,9 @@
 Polymer('filter-by-text-styled', {
 
-  ready: function() {
-    this.super();
-
-    this.filterTextElement = this.shadowRoots['filter-by-text-styled'].querySelector('#filterText');
-  },
-
   filter: function(filterText) {
     this.super(arguments);
 
-    this.filterTextElement.textContent = filterText;
+    this.$.filterText.textContent = filterText;
   }
 
 });
